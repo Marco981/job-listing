21f8f01cbfdb9a4eff55f9fdb1711810ad0567ba
marco981 <marco98x2@gmail.com>
2020-06-11 17:33:39 +0100

    create JobListings component

---
+++ src/components/JobListings/JobListings.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import jobs from '../../data/data.json';
+import JobListing from './JobListing/JobListing';
+import Filters from './Filters/Filters';
+
+import './JobListings.scss';
+
+class JobListings extends Component {
+    state = {
+        jobs: jobs,
+        filters: []
+    };
+
+    addFilter = (language) => {
+        if (!this.state.filters.includes(language)) {
+            this.setState({
+                filters: this.state.filters.concat(language)
+            });
+        }
+    };
+
+    removeFilter = (language) => {
+        this.setState({
+            filters: this.state.filters.filter((f) => {
+                return f !== language;
+            })
+        });
+    };
+
+    filterJobs = () => {
+        const filteredJobs = this.state.jobs.filter((job) => {
+            return this.state.filters.every((lang) => job.languages.includes(lang));
+        });
+
+        return filteredJobs;
+    };
+
+    clearFilters = () => {
+        this.setState({
+            filters: []
+        });
+    };
+
+    render() {
+        return (
+            <div className='job-listings'>
+                {this.state.filters.length > 0 && (
+                    <Filters filters={this.state.filters} remove={this.removeFilter} clearFilters={this.clearFilters} />
+                )}
+                {this.filterJobs().map((job) => {
+                    return <JobListing click={this.addFilter} key={job.id} {...job} />;
+                })}
+            </div>
+        );
+    }
+}
+
+export default JobListings;
