76b98a5a5e956ca3433fe20d18efad59c07ad0f5
marco981 <marco98x2@gmail.com>
2020-06-11 17:34:24 +0100

    create Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { ReactComponent as HeaderBackground } from '../../assets/images/bg-header-desktop.svg';
+
+import './Header.scss';
+export default function Header() {
+    return <div className='Header'>{/* <HeaderBackground /> */}</div>;
+}
